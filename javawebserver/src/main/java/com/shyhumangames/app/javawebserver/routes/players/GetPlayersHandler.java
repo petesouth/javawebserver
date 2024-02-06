4beca43ea268975f3302980c7fbab6e3f57f2dd1
Peter Southwell <pete_south@yahoo.com>
2024-02-06 10:22:23 +0000

    Real mongo values from DB

--- javawebserver/src/main/java/com/shyhumangames/app/javawebserver/routes/players/GetPlayersHandler.java
+++ javawebserver/src/main/java/com/shyhumangames/app/javawebserver/routes/players/GetPlayersHandler.java
@@ -2,21 +2,48 @@ package com.shyhumangames.app.javawebserver.routes.players;
 
 import java.io.IOException;
 import java.io.OutputStream;
+import java.util.logging.Logger;
 
+import org.bson.Document;
+import org.bson.conversions.Bson;
+
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.model.Filters;
+import com.shyhumangames.app.javawebserver.database.mongo.MongoDBService;
 import com.shyhumangames.app.javawebserver.server.HttpHandlerExt;
 import com.sun.net.httpserver.HttpExchange;
 
-
-
 public class GetPlayersHandler implements HttpHandlerExt {
+
+    private static final Logger LOGGER = Logger.getLogger(GetPlayersHandler.class.getName());
 
     @Override
     public void handle(HttpExchange exchange) throws IOException {
+
+        String path = exchange.getRequestURI().getPath();
+        String[] paths = path.split(("/"));
+        int _id = Integer.parseInt(paths[2]);
+        MongoCollection<Document> collection = MongoDBService.getMongoDBService().getCollection("players");
+        Bson filter = Filters.eq("_id", _id);
+
         exchange.getResponseHeaders().set("Content-Type", "application/json");
-        
-        String response = "{ \"status\": 200, \"message\": \"The Players Handler\" }";
-        exchange.sendResponseHeaders(200, response.length());
+
+        // Find the first document that matches the filter.
+        // If expecting multiple results, use `find(filter).iterator()` and handle
+        // accordingly.
+        Document player = collection.find(filter).first();
         OutputStream os = exchange.getResponseBody();
+        String response = "{ \"status\": 404, \"message\": \"Player not found\" }";
+                 
+        if (player != null) {
+            LOGGER.info("Player found with _id: " + _id + " values player: " + player.toJson());
+            response = player.toJson();
+            exchange.sendResponseHeaders(200, response.length());
+        } else {
+            LOGGER.info("No player found with _id: " + _id);
+            exchange.sendResponseHeaders(404, response.length());
+        }
+        
         os.write(response.getBytes());
         os.close();
     }
--- javawebserver/src/main/java/com/shyhumangames/app/javawebserver/routes/ticket/GetTicketsHandler.java
+++ javawebserver/src/main/java/com/shyhumangames/app/javawebserver/routes/ticket/GetTicketsHandler.java
@@ -1,20 +1,49 @@
-package com.shyhumangames.app.javawebserver.routes.ticket;
+package com.shyhumangames.app.javawebserver.routes.players;
 
 import java.io.IOException;
 import java.io.OutputStream;
+import java.util.logging.Logger;
 
+import org.bson.Document;
+import org.bson.conversions.Bson;
+
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.model.Filters;
+import com.shyhumangames.app.javawebserver.database.mongo.MongoDBService;
 import com.shyhumangames.app.javawebserver.server.HttpHandlerExt;
 import com.sun.net.httpserver.HttpExchange;
 
 public class GetTicketsHandler implements HttpHandlerExt {
 
+    private static final Logger LOGGER = Logger.getLogger(GetTicketsHandler.class.getName());
+
     @Override
     public void handle(HttpExchange exchange) throws IOException {
+
+        String path = exchange.getRequestURI().getPath();
+        String[] paths = path.split(("/"));
+        int _id = Integer.parseInt(paths[2]);
+        MongoCollection<Document> collection = MongoDBService.getMongoDBService().getCollection("tickets");
+        Bson filter = Filters.eq("_id", _id);
+
         exchange.getResponseHeaders().set("Content-Type", "application/json");
-        
-        String response = "{ \"status\": 200, \"message\": \"The Ticket Handler\" }";
-        exchange.sendResponseHeaders(200, response.length());
+
+        // Find the first document that matches the filter.
+        // If expecting multiple results, use `find(filter).iterator()` and handle
+        // accordingly.
+        Document ticket = collection.find(filter).first();
         OutputStream os = exchange.getResponseBody();
+        String response = "{ \"status\": 404, \"message\": \"Player not found\" }";
+                 
+        if (ticket != null) {
+            LOGGER.info("Ticket found with _id: " + _id + " values ticket: " + ticket.toJson());
+            response = ticket.toJson();
+            exchange.sendResponseHeaders(200, response.length());
+        } else {
+            LOGGER.info("No player found with _id: " + _id);
+            exchange.sendResponseHeaders(404, response.length());
+        }
+        
         os.write(response.getBytes());
         os.close();
     }
